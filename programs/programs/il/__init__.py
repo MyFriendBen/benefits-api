6fed9177e38d5e17234d4b33197ce90a540236bd
Pat Wey <pat@myfriendben.org>
2025-08-08 14:35:57 -0700

    IL: Add FamilyCare
    Implements FamilyCare as separate calculator with base Medicaid eligibility
    check. Covers pregnant women and caretaker relatives of children ≤18.
    
    - Create IlMedicaid base class in PolicyEngine with IL-specific amounts
    - Add FamilyCare calculator checking medicaid_eligible() + specific criteria
    - Support 138% FPL income limit (includes 5% disregard)
    - Register il_medicaid in PE and il_family_care in IL calculators
    
    https://github.com/Gary-Community-Ventures/benefits-calculator/issues/1888

--- programs/programs/il/__init__.py
+++ programs/programs/il/__init__.py
@@ -1,3 +1,6 @@
+from .medicaid.family_care.calculator import FamilyCare
 from ..calc import ProgramCalculator
 
-il_calculators: dict[str, type[ProgramCalculator]] = {}
+il_calculators: dict[str, type[ProgramCalculator]] = {
+    "il_family_care": FamilyCare,
+}
