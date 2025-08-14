7ed0e7f0220b39e0df4f625f48a4d1a3990b368a
Pat Wey <pat@myfriendben.org>
2025-08-14 09:30:03 -0600

    IL: Add Moms and Babies Program (#1102)
    Implements Moms and Babies program. Provides coverage
    for pregnant women and families with infants under 1
    year at higher income levels.
    
    - Add MomsAndBabies calculator
    - Register il_moms_and_babies in IL calculators
    
    https://github.com/Gary-Community-Ventures/benefits-calculator/issues/1888

--- programs/programs/il/__init__.py
+++ programs/programs/il/__init__.py
@@ -1,6 +1,8 @@
 from .medicaid.family_care.calculator import FamilyCare
+from .medicaid.moms_and_babies.calculator import MomsAndBabies
 from ..calc import ProgramCalculator
 
 il_calculators: dict[str, type[ProgramCalculator]] = {
     "il_family_care": FamilyCare,
+    "il_moms_and_babies": MomsAndBabies,
 }
