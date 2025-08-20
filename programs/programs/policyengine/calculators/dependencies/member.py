d20a8fa52f419f80587510ce23f021c655e8d627
ming <ming@codethedream.org>
2025-08-20 13:08:08 -0700

    send member SSI value in pe input data

--- programs/programs/policyengine/calculators/dependencies/member.py
+++ programs/programs/policyengine/calculators/dependencies/member.py
@@ -108,6 +108,9 @@ class Medicaid(Member):
 class Ssi(Member):
     field = "ssi"
 
+    def value(self):
+        return self.member.calc_gross_income("yearly", ["sSI"])
+
 
 class IsDisabledDependency(Member):
     field = "is_disabled"
