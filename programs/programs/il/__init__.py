76896ed96e0e7e495ffd0e3acb6349537a0949f0
Pat Wey <pat@myfriendben.org>
2025-08-20 20:20:09 -0700

    IL: Add BAP Program (#1120)
    - Add IlBenefitAccess program
    - Add program configuration
    - Update IL calculators config
    
    https://github.com/MyFriendBen/benefits-api/issues/1117

--- programs/programs/il/__init__.py
+++ programs/programs/il/__init__.py
@@ -2,6 +2,7 @@ from .medicaid.family_care.calculator import FamilyCare
 from .medicaid.moms_and_babies.calculator import MomsAndBabies
 from .medicaid.aca_adults.calculator import AcaAdults
 from .medicaid.all_kids.calculator import AllKids
+from .bap.calculator import IlBenefitAccess
 from ..calc import ProgramCalculator
 
 il_calculators: dict[str, type[ProgramCalculator]] = {
@@ -9,4 +10,5 @@ il_calculators: dict[str, type[ProgramCalculator]] = {
     "il_moms_and_babies": MomsAndBabies,
     "il_aca_adults": AcaAdults,
     "il_all_kids": AllKids,
+    "il_bap": IlBenefitAccess,
 }
