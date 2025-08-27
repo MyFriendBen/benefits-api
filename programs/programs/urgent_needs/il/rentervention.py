3e604c0c34d2cfca53808dcca541b804511ec150
IrynaKolh <iryna.kolhanova@gmail.com>
2025-08-27 12:29:44 -0400

    update needs_housing_help variable name

--- programs/programs/urgent_needs/il/rentervention.py
+++ programs/programs/urgent_needs/il/rentervention.py
@@ -4,9 +4,9 @@ from ..base import UrgentNeedFunction
 class Rentervention(UrgentNeedFunction):
     def eligible(self):
         # Condition 1: Household needs housing/utilities help
-        need_help = self.screen.needs_housing_help
+        needs_housing_help = self.screen.needs_housing_help
 
         # Condition 2: Household has rent expense
         has_rent = self.screen.has_expense(["rent"])
 
-        return need_help and has_rent
+        return needs_housing_help and has_rent
