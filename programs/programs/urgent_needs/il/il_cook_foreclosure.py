0f65e83df66d88e1f64c27a0276f1061c414cca3
Katie Brey <katie@codethedream.org>
2025-08-27 12:25:51 -0400

    remove county condition from function, setting in django instead

--- programs/programs/urgent_needs/il/il_cook_foreclosure.py
+++ programs/programs/urgent_needs/il/il_cook_foreclosure.py
@@ -2,14 +2,9 @@ from ..base import UrgentNeedFunction
 
 
 class IlCookForeclosure(UrgentNeedFunction):
-    dependencies = ["county"]
-    county = "Cook"
 
     def eligible(self):
-        # county
-        county_eligible = self.screen.county == self.county
-
         # expenses
         has_mortgage = self.screen.has_expense(["mortgage"])
 
-        return county_eligible and has_mortgage
+        return has_mortgage
