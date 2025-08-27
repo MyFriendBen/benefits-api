633fd9bc1e2714bb1653692d97927da286d9692b
Sonali Bedge <sonali@codethedream.org>
2025-08-27 09:48:06 -0700

    formatted

--- programs/programs/urgent_needs/il/__init__.py
+++ programs/programs/urgent_needs/il/__init__.py
@@ -1,7 +1,6 @@
 from .il_early_intervention import IlEarlyIntervention
 from .il_beacon import il_beacon
 from programs.programs.urgent_needs.il.rentervention import Rentervention
-from programs.programs.urgent_needs.il.rentervention import Rentervention
 from ..base import UrgentNeedFunction
 
 il_urgent_need_functions: dict[str, type[UrgentNeedFunction]] = {
