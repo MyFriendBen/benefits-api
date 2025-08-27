5afe5c2fc4406f54a212e6ed725e51ed1d88da0c
Katie Brey <kbrey@users.noreply.github.com>
2025-08-27 12:04:13 -0400

    adding il early intervention urgent need function (#1130)

--- programs/programs/urgent_needs/il/__init__.py
+++ programs/programs/urgent_needs/il/__init__.py
@@ -1,3 +1,4 @@
+from .il_early_intervention import IlEarlyIntervention
 from ..base import UrgentNeedFunction
 
-il_urgent_need_functions: dict[str, type[UrgentNeedFunction]] = {}
+il_urgent_need_functions: dict[str, type[UrgentNeedFunction]] = {"il_early_interv": IlEarlyIntervention}
