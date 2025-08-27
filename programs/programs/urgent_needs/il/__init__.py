975263c27acf65e3f1fce952709734adb6586cc1
Sonali Bedge <sonali@codethedream.org>
2025-08-27 10:43:41 -0700

    Made small change

--- programs/programs/urgent_needs/il/__init__.py
+++ programs/programs/urgent_needs/il/__init__.py
@@ -1,10 +1,8 @@
 from .il_early_intervention import IlEarlyIntervention
 from .il_beacon import il_beacon
-from programs.programs.urgent_needs.il.rentervention import Rentervention
 from ..base import UrgentNeedFunction
 
 il_urgent_need_functions: dict[str, type[UrgentNeedFunction]] = {
     "il_early_interv": IlEarlyIntervention,
     "il_beacon": il_beacon,
-    "il_rentervention": Rentervention,
 }
