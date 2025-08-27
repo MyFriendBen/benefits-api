7948db82140ade2268a361a78955b2c30e88a3b5
Sonali Bedge <sonali@codethedream.org>
2025-08-27 10:16:16 -0700

    Removed need conditions from this urgent need function

--- programs/programs/urgent_needs/il/il_beacon.py
+++ programs/programs/urgent_needs/il/il_beacon.py
@@ -7,9 +7,6 @@ class il_beacon(UrgentNeedFunction):
     max_age = 21
 
     def eligible(self):
-        # Check if household has marked mental health or child development help
-        needs_mental_health = self.screen.needs_mental_health_help
-        needs_child_dev = self.screen.needs_child_dev_help
 
         # Check if at least one household member is between 5-21
         has_eligible_age_member = False
@@ -19,4 +16,4 @@ class il_beacon(UrgentNeedFunction):
                 break
 
         # Return True if both conditions are met
-        return (needs_mental_health or needs_child_dev) and has_eligible_age_member
+        return has_eligible_age_member
