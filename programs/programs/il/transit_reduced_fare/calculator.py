a90e4daabcd01dfc8fbaa1cff44d8afe719477f5
Pat Wey <pat@myfriendben.org>
2025-09-10 15:39:09 -0700

    Update LTRF eligibility logic
    - Remove income check
    - Add Medicare presumptive eligibility check to member_eligible
    - Refactor IlTransportationMixin based on updated shared behavior

--- programs/programs/il/transit_reduced_fare/calculator.py
+++ programs/programs/il/transit_reduced_fare/calculator.py
@@ -1,10 +1,9 @@
-from programs.programs.il.bap.calculator import IlBenefitAccess
 from programs.programs.calc import ProgramCalculator, Eligibility, MemberEligibility
 from programs.programs.mixins import IlTransportationMixin
 
 
 class IlTransitReducedFare(IlTransportationMixin, ProgramCalculator):
-    dependencies = IlTransportationMixin.dependencies + ["county"]
+    dependencies = ["age", "visually_impaired", "disabled", "county"]
     eligible_counties = [
         "Cook",
         "DeKalb",
@@ -26,17 +25,12 @@ class IlTransitReducedFare(IlTransportationMixin, ProgramCalculator):
     def household_eligible(self, e: Eligibility):
         e.condition(not self.screen.has_benefit("il_transit_reduced_fare"))
 
-        presumptive_eligible = self.check_presumptive_eligibility()
+        # Note: SSI/SSDI presumptive eligibility is not checked here because
+        # there are currently no eligibility requirements to bypass. County eligibility is the
+        # only household-level requirement. Medicare eligibility is checked
+        # at the member level instead.
 
-        household_size = self.screen.household_size
-        gross_income = int(self.screen.calc_gross_income("yearly", ["all"]))
-        bap_income_limit = IlBenefitAccess.income_limit_by_household_size[min(household_size, 3)]
-        income_eligible = gross_income > bap_income_limit
-
-        county = self.screen.county
-        county_eligible = county in self.eligible_counties
-
-        e.condition(county_eligible and (presumptive_eligible or income_eligible))
+        e.condition(self.screen.county in self.eligible_counties)
 
     def member_eligible(self, e: MemberEligibility):
         member = e.member
@@ -48,4 +42,8 @@ class IlTransitReducedFare(IlTransportationMixin, ProgramCalculator):
         has_eligible_disability = member.visually_impaired or member.disabled
         disability_eligible = has_minimum_age_with_disability and has_eligible_disability
 
-        e.condition(age_eligible or disability_eligible)
+        # Check for Medicare as presumptive eligibility
+        has_medicare = member.insurance.has_insurance_types(["medicare"])
+
+        # Member is eligible if they have Medicare OR meet age/disability requirements
+        e.condition(has_medicare or age_eligible or disability_eligible)
