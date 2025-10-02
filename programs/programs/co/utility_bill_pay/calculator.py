e2f081568d04612879121ff6ee081801ba330102
Sonali Bedge <sonali@codethedream.org>
2025-10-02 11:47:58 -0700

    reformatted

--- programs/programs/co/utility_bill_pay/calculator.py
+++ programs/programs/co/utility_bill_pay/calculator.py
@@ -3,6 +3,7 @@ from programs.co_county_zips import counties_from_screen
 from programs.programs.calc import Eligibility, ProgramCalculator
 import programs.programs.messages as messages
 
+
 class IncomeLimitsCache(GoogleSheetsCache):
     sheet_id = "1ZzQYhULtiP61crj0pbPjhX62L1TnyAisLcr_dQXbbFg"
     range_name = "A2:K"
@@ -10,24 +11,25 @@ class IncomeLimitsCache(GoogleSheetsCache):
 
     def update(self):
         data = super().update()
-    
+
         return {self._format_county(r[0]): self._format_amounts(r[1:9]) for r in data}
-    
+
     @staticmethod
     def _format_county(county: str):
         return county.strip() + " County"
-    
+
     @staticmethod
     def _format_amounts(amounts: list[str]):
-        return [float(a.strip().replace("$","").replace(",","")) for a in amounts]
+        return [float(a.strip().replace("$", "").replace(",", "")) for a in amounts]
+
 
 class UtilityBillPay(ProgramCalculator):
-    income_limits = IncomeLimitsCache()    
+    income_limits = IncomeLimitsCache()
     presumptive_eligibility = ("snap", "ssi", "andcs", "tanf", "wic", "chp")
     member_presumptive_eligibility = ("co_medicaid", "emergency_medicaid")
     amount = 400
 
-    def household_eligible(self, e: Eligibility):        
+    def household_eligible(self, e: Eligibility):
         # has other programs
         presumptive_eligible = False
         for benefit in self.presumptive_eligibility:
@@ -49,11 +51,11 @@ class UtilityBillPay(ProgramCalculator):
         income_limits = []
         for county in counties:
             income_limits.append(self.income_limits.fetch()[county][self.screen.household_size - 1])
-        income_limit =  min(income_limits)
+        income_limit = min(income_limits)
 
         income = int(self.screen.calc_gross_income("yearly", ["all"]))
         income_eligible = income <= income_limit
-        
+
         e.condition(income_eligible, messages.income(income, income_limit))
 
         e.condition(presumptive_eligible)
