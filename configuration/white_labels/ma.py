29672a628143207ff5e081feede55f5d2e1fa82d
Sonali Bedge <sonali@codethedream.org>
2025-10-09 17:06:29 -0700

    Renamed icon name from Child_development_ma to Youth_development.

--- configuration/white_labels/ma.py
+++ configuration/white_labels/ma.py
@@ -64,10 +64,10 @@ class MaConfigurationData(ConfigurationData):
                 "_default_message": "A challenge you or your child would like to talk about",
             },
         },
-        "childDevelopment": {
-            "icon": {"_icon": "Child_development", "_classname": "option-card-icon"},
+        "youthDevelopment": {
+            "icon": {"_icon": "Youth_development", "_classname": "option-card-icon"},
             "text": {
-                "_label": "acuteConditionOptions.childDevelopment_ma",
+                "_label": "acuteConditionOptions.youthDevelopment",
                 "_default_message": "Concern about your child's development (ages 0 - 21)",
             },
         },
