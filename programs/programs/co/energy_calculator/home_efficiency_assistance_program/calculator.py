d9a9b3f18811286d0b2544e96606fad3f0d8de1f
Caton Hauer <catonhauer@Catons-MacBook-Pro.local>
2025-10-16 09:35:13 -0600

    progress

--- programs/programs/co/energy_calculator/home_efficiency_assistance_program/calculator.py
+++ programs/programs/co/energy_calculator/home_efficiency_assistance_program/calculator.py
@@ -15,22 +15,22 @@ class EnergyCalculatorHomeEfficiencyAssistance(ProgramCalculator):
     def household_eligible(self, e: Eligibility):
         energy_calculator_screen = self.screen.energy_calculator
 
-        # eligible for presumptive eligbility program
-        has_another_program = any(
+        presumed_eligible = any(
             self.data[program].eligible 
             for program in self.presumptive_eligibility 
             if program in self.data
         )
-        e.condition(has_another_program)
 
-        # has utility providers
-        e.condition(energy_calculator_screen.has_utility_provider(self.utility_providers))
+        if presumed_eligible:
+            # if presumptive eligbility via LEAP, done
+            e.condition(presumed_eligible, )
+        else:
+            # otherwise, must meet all 3 conditions
+            e.condition(energy_calculator_screen.has_utility_provider(self.utility_providers))
 
-        # must be homeowner
-        e.condition((energy_calculator_screen.is_home_owner))
+            e.condition((energy_calculator_screen.is_home_owner))
 
-        # income
-        income_limit = ami.get_screen_ami(self.screen, self.ami_percent, self.program.year.period)
-        income = int(self.screen.calc_gross_income("yearly", ["all"]))
-        income_eligible = income <= income_limit
-        e.condition(income_eligible, messages.income(income, income_limit))
+            income_limit = ami.get_screen_ami(self.screen, self.ami_percent, self.program.year.period)
+            income = int(self.screen.calc_gross_income("yearly", ["all"]))
+            income_eligible = income <= income_limit
+            e.condition(income_eligible, messages.income(income, income_limit))
