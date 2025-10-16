720243067ad3208e6ff5623fe871d8e2c5fe13b8
Sonali Bedge <sonali@codethedream.org>
2025-10-16 13:57:40 -0700

    Added Project COPE program for Colorado Springs Utilities customers with past-due bills or disconnected service.

--- programs/programs/co/energy_calculator/assistance_programs/project_cope/calculator.py
+++ programs/programs/co/energy_calculator/assistance_programs/project_cope/calculator.py
@@ -1,29 +1,16 @@
 from programs.programs.calc import Eligibility, ProgramCalculator
-from programs.programs.co.energy_calculator.util import has_renter_expenses
 
 
 class EnergyCalculatorProjectCOPE(ProgramCalculator):
-    amount = 1
     dependencies = ["energy_calculator"]
-    electricity_providers = ["co-colorado-springs-utilities"]
-    gas_providers = ["co-colorado-springs-utilities"]
+    utility_providers = ["co-colorado-springs-utilities"]
 
     def household_eligible(self, e: Eligibility):
-        # utility provider check
-        # e.condition(self.screen.energy_calculator.has_utility_provider(self.electricity_providers + self.gas_providers))
-        print(self.screen.energy_calculator.has_utility_provider(self.electricity_providers))
-        e.condition(self.screen.energy_calculator.has_utility_provider(self.electricity_providers))
-        print(e.condition(self.screen.energy_calculator.has_utility_provider(self.electricity_providers)))
-        print(e.condition(
-             self.screen.energy_calculator.electricity_is_disconnected
-        #     or self.screen.energy_calculator.has_past_due_energy_bills
-        ))
-        print(self.screen.energy_calculator.electricity_is_disconnected)
-        # past due or disconnected
-        # e.condition(
-        #     self.screen.energy_calculator.electricity_is_disconnected
-        #     or self.screen.energy_calculator.has_past_due_energy_bills
-        # )
+        # utility provider check (electric or gas)
+        e.condition(self.screen.energy_calculator.has_utility_provider(self.utility_providers))
 
-        # no renters without expenses
-        # e.condition(has_renter_expenses(self.screen))
+        # past due or disconnected
+        e.condition(
+            self.screen.energy_calculator.electricity_is_disconnected
+            or self.screen.energy_calculator.has_past_due_energy_bills
+        )
