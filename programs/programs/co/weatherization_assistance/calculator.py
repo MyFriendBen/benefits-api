f594109d91f36a8acdd669c8c4c7aa5a9b4b642c
Caton Pace Hauer <84205874+catonph@users.noreply.github.com>
2025-10-22 14:22:57 -0600

    MFB393: fix UtilitybillHelp logic (#1200)
    * refactor into class and methods
    
    * logic updates
    
    * ran black formatting
    
    * black formatting round 2
    
    * fix logging
    
    * make the log a warning not an error
    
    ---------
    
    Co-authored-by: Caton Hauer <catonhauer@Catons-MacBook-Pro.local>

--- programs/programs/co/weatherization_assistance/calculator.py
+++ programs/programs/co/weatherization_assistance/calculator.py
@@ -1,13 +1,18 @@
 from programs.programs.calc import Eligibility, ProgramCalculator
 import programs.programs.messages as messages
-from programs.programs.co.energy_programs_shared.income_validation import get_income_limit
+from integrations.services.income_limits import income_limits_cache
 from typing import ClassVar
 
 
 class WeatherizationAssistance(ProgramCalculator):
     presumptive_eligibility = ("andcs", "ssi", "snap", "leap", "tanf")
     amount = 350
-    dependencies: ClassVar[list[str]] = ["household_size", "income_amount", "income_frequency", "county"]
+    dependencies: ClassVar[list[str]] = [
+        "household_size",
+        "income_amount",
+        "income_frequency",
+        "county",
+    ]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -22,8 +27,8 @@ class WeatherizationAssistance(ProgramCalculator):
         if presumed_eligibility:
             e.condition(presumed_eligibility, messages.presumed_eligibility())
         else:
-            # check income limit
-            income_limit = get_income_limit(self.screen)
+            # check income limit, expenses, and utility provider
+            income_limit = income_limits_cache.get_income_limit(self.screen)
             if income_limit:
                 user_income = int(self.screen.calc_gross_income("yearly", ["all"]))
                 income_eligible = user_income <= income_limit
@@ -32,11 +37,11 @@ class WeatherizationAssistance(ProgramCalculator):
                 # no income limit data
                 e.condition(False, messages.income_limit_unknown())
 
-        # rent or mortgage expense
-        e.condition(self._has_expense())
+            # rent or mortgage expense
+            e.condition(self._has_expense())
 
-        # utility providers
-        e.condition(self._has_utility_provider())
+            # utility provider
+            e.condition(self._has_utility_provider())
 
     def _has_expense(self):
         return self.screen.has_expense(["rent", "mortgage"])
