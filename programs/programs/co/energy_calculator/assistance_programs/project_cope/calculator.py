e03da89dbac0238212459e74731570505adec8db
Sonali Bedge <sonali@codethedream.org>
2025-10-27 13:19:38 -0700

    added amount=1 in project cope calculator.py file.

--- programs/programs/co/energy_calculator/assistance_programs/project_cope/calculator.py
+++ programs/programs/co/energy_calculator/assistance_programs/project_cope/calculator.py
@@ -2,15 +2,14 @@ from programs.programs.calc import Eligibility, ProgramCalculator
 
 
 class EnergyCalculatorProjectCOPE(ProgramCalculator):
+    amount = 1
     dependencies = ["energy_calculator"]
     utility_providers = ["co-colorado-springs-utilities"]
 
     def household_eligible(self, e: Eligibility):
         # utility provider check (electric or gas)
         e.condition(self.screen.energy_calculator.has_utility_provider(self.utility_providers))
-        print(self.screen.energy_calculator.has_utility_provider(self.utility_providers))
-        print(self.screen.energy_calculator.electricity_is_disconnected)
-        print(self.screen.energy_calculator.has_past_due_energy_bills)
+
         # past due or disconnected
         e.condition(
             self.screen.energy_calculator.electricity_is_disconnected
