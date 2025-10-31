5706d908036dbfaeb75aebe3d35932223b8d94fd
Sonali Bedge <sonali@codethedream.org>
2025-10-31 11:11:54 -0700

    Added family resource centers to more help button for ma white label.

--- configuration/white_labels/ma.py
+++ configuration/white_labels/ma.py
@@ -25,6 +25,14 @@ class MaConfigurationData(ConfigurationData):
                 "link": "https://mass211.org/",
             },
             {
+                "name": {"_default_message": "Family Resource Center", "_label": "moreHelp.ma.resource_name1"},
+                "description": {
+                    "_default_message": "If you have a child ages 0 to 18, your local family resource center may be able to connect you to other resources and support services. Visit a center near you.",
+                    "_label": "moreHelp.ma.resource_description1",
+                },
+                "link": "https://www.frcma.org/",
+            },
+            {
                 "name": {"_label": "moreHelp.ma.masscap.name", "_default_message": "MASSCAP Community Action Agencies"},
                 "description": {
                     "_label": "moreHelp.ma.masscap.phone",
