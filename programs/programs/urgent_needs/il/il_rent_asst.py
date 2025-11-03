8cc5e4fb8e348fd14690d488c60fdc8f34beccf2
Sonali Bedge <sonali@codethedream.org>
2025-11-03 14:12:29 -0800

    Run formate.

--- programs/programs/urgent_needs/il/il_rent_asst.py
+++ programs/programs/urgent_needs/il/il_rent_asst.py
@@ -5,22 +5,16 @@ from integrations.services.income_limits import ami
 class IlRenterAssistance(UrgentNeedFunction):
     ami_percent = "80%"
     dependencies = ["income_amount", "income_frequency", "household_size", "county"]
-    
+
     def eligible(self):
         """
         Return True if the household is at or below 80% AMI using HUD 2025 income limits
-        """           
-        
-        income_limit = ami.get_screen_ami(
-            self.screen, 
-            self.ami_percent, 
-            self.urgent_need.year.period, 
-            limit_type="il"
-        )   
-            
+        """
+
+        income_limit = ami.get_screen_ami(self.screen, self.ami_percent, self.urgent_need.year.period, limit_type="il")
+
         income = self.screen.calc_gross_income("yearly", ["all"])
         needs_housing_help = self.screen.needs_housing_help
-        has_rent = self.screen.has_expense(["rent"])       
-        
-        
+        has_rent = self.screen.has_expense(["rent"])
+
         return needs_housing_help and has_rent and income <= income_limit
