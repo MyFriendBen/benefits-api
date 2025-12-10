56365ff4b28e834eae9107801ab57de787749f23
Iryna Kolhanova <96072009+IrynaKolh@users.noreply.github.com>
2025-12-10 15:50:24 -0500

    NC: Fix estimated savings calculations in NC's Crisis Intervention Programs (#1285)

--- programs/programs/nc/nc_crisis_intervention/calculator.py
+++ programs/programs/nc/nc_crisis_intervention/calculator.py
@@ -5,12 +5,7 @@ import programs.programs.messages as messages
 class NCCrisisIntervention(ProgramCalculator):
     expenses = ["rent", "mortgage", "heating", "cooling"]
     fpl_percent = 1.5
-    large_household_size = 4
-    max_value_fpl_percent = 0.5
-    small_household_low_income_value = 400
-    small_household_large_income_value = 300
-    large_household_low_income_value = 500
-    large_household_large_income_value = 400
+    amount = 600
 
     dependencies = [
         "household_size",
@@ -29,21 +24,3 @@ class NCCrisisIntervention(ProgramCalculator):
         gross_income = self.screen.calc_gross_income("yearly", ["all"])
         income_limit = int(self.fpl_percent * self.program.year.as_dict()[household_size])
         e.condition(gross_income < income_limit, messages.income(gross_income, income_limit))
-
-    def household_value(self):
-        household_size = self.screen.household_size
-        gross_income = self.screen.calc_gross_income("yearly", ["all"])
-        income_limit = int(self.program.year.as_dict()[household_size])
-
-        if household_size < self.large_household_size:  # 1-3 person household
-            if gross_income <= income_limit * self.max_value_fpl_percent:  # 0-50% FPL
-                return self.small_household_low_income_value  # $400/month
-            elif gross_income <= income_limit:  # 51%+ FPL
-                return self.small_household_large_income_value  # $300/month
-        else:  # 4+ person household
-            if gross_income <= income_limit * self.max_value_fpl_percent:  # 0-50% FPL
-                return self.large_household_low_income_value  # $500/month
-            elif gross_income <= income_limit:  # 51%+ FPL
-                return self.large_household_large_income_value  # $400/month
-
-        return 0
