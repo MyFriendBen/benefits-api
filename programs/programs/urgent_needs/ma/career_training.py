7cb329635cd28e2678deb188e9b9791442df699e
IrynaKolh <iryna.kolhanova@gmail.com>
2025-12-17 23:00:02 -0500

    Add MA: Just A Start - Career Training & Workforce Programs Additional Resource

---
+++ programs/programs/urgent_needs/ma/career_training.py
@@ -0,0 +1,26 @@
+from ..base import UrgentNeedFunction
+
+
+class CareerTrainingAndWorkforce(UrgentNeedFunction):
+    dependencies = ["age", "income_amount", "income_frequency", "household_size", "needs_job_resources"]
+    min_age = 18
+    fpl_percent = 2
+
+    def eligible(self):
+        """
+        Career Training and Workforce Development
+        Programs and services to help individuals gain skills and find employment.
+        """
+
+        # Condition 1: Individual is 18+
+        is_adult = self.screen.num_adults(age_max=self.min_age) > 0
+
+        # Condition 2: income less then 200% FPL
+        income_limit = self.urgent_need.year.as_dict()[self.screen.household_size] * self.fpl_percent
+        income = self.screen.calc_gross_income("yearly", ["all"])
+        income_eligible = income <= income_limit
+
+        # Condition 3: Household needs job resources
+        needs_job_resources = self.screen.needs_job_resources
+
+        return is_adult and income_eligible and needs_job_resources
