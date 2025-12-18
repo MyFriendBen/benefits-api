9786d77590a87c84aab842cc82d6ff8b6248f2cc
IrynaKolh <iryna.kolhanova@gmail.com>
2025-12-18 13:49:32 -0500

    update after review

--- programs/programs/urgent_needs/ma/senior_center.py
+++ programs/programs/urgent_needs/ma/senior_center.py
@@ -2,7 +2,7 @@ from ..base import UrgentNeedFunction
 
 
 class CambridgeSeniorCenter(UrgentNeedFunction):
-    dependencies = ["age", "county", "needs_legal_services"]
+    dependencies = ["age", "county"]
     min_age = 60
     eligible_city = "Cambridge"
 
@@ -15,14 +15,10 @@ class CambridgeSeniorCenter(UrgentNeedFunction):
         Eligibility:
         - Age: 60+
         - Location: Cambridge residents
-        - Needs: Legal services
         """
 
-        # Condition 1: Household needs legal services
-        needs_legal_help = self.screen.needs_legal_services
-
-        # Condition 2:  Cambridge residents age 60+.
+        # Condition 1:  Cambridge residents age 60+.
         is_senior = self.screen.num_adults(age_max=self.min_age) > 0
         is_cambridge = self.screen.county == self.eligible_city
 
-        return is_senior and is_cambridge and needs_legal_help
+        return is_senior and is_cambridge
