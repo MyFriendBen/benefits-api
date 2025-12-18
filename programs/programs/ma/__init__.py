d3d8de25d7f769921f53b90e02575f87a8f476ac
Pat Wey <pat@myfriendben.org>
2025-12-18 14:13:02 -0800

    Add MA: Door2Door Transportation (#1302)
    * Add MA: Door2Door Transportation
    
    - Update white label config
    - Add initial config
    - Add calculator
    - Add has_ma_door_to_door
    - Update screener/serializer
    - Add validation test cases
    
    https://linear.app/myfriendben/issue/MFB-529/ma-add-door2door-transportation-scm
    
    * Add source
    
    * Fix formatting
    
    * Add merge migration

--- programs/programs/ma/__init__.py
+++ programs/programs/ma/__init__.py
@@ -1,5 +1,6 @@
 from .homebridge.calculator import MaHomeBridge
 from .dhsp_afterschool.calculator import MaDhspAfterschool
+from .door2door.calculator import MaDoorToDoor
 from ..calc import ProgramCalculator
 from .cha.calculator import Cha
 
@@ -7,4 +8,5 @@ ma_calculators: dict[str, type[ProgramCalculator]] = {
     "ma_homebridge": MaHomeBridge,
     "ma_cha": Cha,
     "ma_dhsp_afterschool": MaDhspAfterschool,
+    "ma_door_to_door": MaDoorToDoor,
 }
