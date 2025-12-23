18a0e066c62b64d098920e5a4d9f761a8c6a7eca
nihalerdal <nihalerdall@gmail.com>
2025-12-22 16:46:08 -0800

    Add MA: CEOC - College & FAFSA Support Additional Resource

---
+++ programs/programs/urgent_needs/ma/college_and_fafsa_support.py
@@ -0,0 +1,22 @@
+from ..base import UrgentNeedFunction
+
+
+class CollegeAndFafsaSupport(UrgentNeedFunction):
+    dependencies = [ "county", "student"]
+    eligible_city = "Cambridge"
+
+    def eligible(self):
+        """
+        CEOC - College & FAFSA Support
+        Helps students complete FAFSA and access college financial aid.
+        Cambridge resident or student. No income limit; affects aid eligibility.
+
+        """
+
+        # Condition 1:  Cambridge residents
+        is_cambridge = self.screen.county == self.eligible_city
+
+        # Condition 2:  Student
+        is_student = self.screen.household_members.filter(student=True).exists() 
+
+        return is_cambridge or is_student
