2130711071eb6d84b495439f2ca566f586c89646
nihalerdal <nihalerdall@gmail.com>
2026-01-07 11:54:42 -0800

    resolve merge conflict

--- programs/programs/urgent_needs/tx/__init__.py
+++ programs/programs/urgent_needs/tx/__init__.py
@@ -1,4 +1,8 @@
 from ..base import UrgentNeedFunction
+from .early_intervention import EarlyIntervention
 from .workforce_solutions import WorkforceSolutions
 
-tx_urgent_need_functions: dict[str, type[UrgentNeedFunction]] = {"tx_workforce_solutions": WorkforceSolutions}
+tx_urgent_need_functions: dict[str, type[UrgentNeedFunction]] = {
+    "tx_early_intervention": EarlyIntervention,
+    "tx_workforce_solutions": WorkforceSolutions,
+}
