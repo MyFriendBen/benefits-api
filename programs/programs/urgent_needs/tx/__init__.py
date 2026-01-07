7e11e5bb430aba455eb89d4d1b0cf33e25d82dbd
nihalerdal <nihalerdall@gmail.com>
2026-01-07 09:38:16 -0800

    TX Additional Resources: Texas Workforce Commission – Job Training / Workforce Solutions

--- programs/programs/urgent_needs/tx/__init__.py
+++ programs/programs/urgent_needs/tx/__init__.py
@@ -1,3 +1,4 @@
 from ..base import UrgentNeedFunction
+from .workforce_solutions import WorkforceSolutions
 
-tx_urgent_need_functions: dict[str, type[UrgentNeedFunction]] = {}
+tx_urgent_need_functions: dict[str, type[UrgentNeedFunction]] = {"tx_workforce_solutions": WorkforceSolutions}
