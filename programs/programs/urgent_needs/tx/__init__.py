6fd550ec3db03dbd5b516a097557dff26630b308
Iryna Kolhanova <96072009+IrynaKolh@users.noreply.github.com>
2026-01-07 14:45:29 -0500

    Add TX: Additional Resources: Early Childhood Intervention (ECI) – Texas HHSC (#1317)
    * TX Additional Resources: Early Childhood Intervention (ECI) – Texas HHSC

--- programs/programs/urgent_needs/tx/__init__.py
+++ programs/programs/urgent_needs/tx/__init__.py
@@ -1,3 +1,6 @@
 from ..base import UrgentNeedFunction
+from .early_intervention import EarlyIntervention
 
-tx_urgent_need_functions: dict[str, type[UrgentNeedFunction]] = {}
+tx_urgent_need_functions: dict[str, type[UrgentNeedFunction]] = {
+    "tx_early_intervention": EarlyIntervention,
+}
