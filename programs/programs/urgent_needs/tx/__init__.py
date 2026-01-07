e8232071d51d3b84ca07e2ce7b1863250ab5d445
RomannaBidnyk <romanna.bidnyk@gmail.com>
2026-01-07 16:18:27 -0500

    MFB-495: Added TX Additional Resources: Feeding Texas

--- programs/programs/urgent_needs/tx/__init__.py
+++ programs/programs/urgent_needs/tx/__init__.py
@@ -1,6 +1,8 @@
 from ..base import UrgentNeedFunction
 from .early_intervention import EarlyIntervention
+from .feeding_texas import FeedingTexas
 
 tx_urgent_need_functions: dict[str, type[UrgentNeedFunction]] = {
     "tx_early_intervention": EarlyIntervention,
+    "tx_feeding_texas": FeedingTexas,
 }
