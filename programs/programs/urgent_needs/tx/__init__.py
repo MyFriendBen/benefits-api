c65d8e96091466877f652c73aeb4d6247eca94b7
ming <ming@codethedream.org>
2026-01-07 13:38:24 -0800

    add urgent need here for texas

--- programs/programs/urgent_needs/tx/__init__.py
+++ programs/programs/urgent_needs/tx/__init__.py
@@ -1,8 +1,10 @@
 from ..base import UrgentNeedFunction
 from .early_intervention import EarlyIntervention
 from .workforce_solutions import WorkforceSolutions
+from .here_for_texas import HereForTexas
 
 tx_urgent_need_functions: dict[str, type[UrgentNeedFunction]] = {
     "tx_early_intervention": EarlyIntervention,
     "tx_workforce_solutions": WorkforceSolutions,
+    "tx_here_for_texas": HereForTexas,
 }
