b1cb86a534edede62fedc1ab5a109dfaef20f490
IrynaKolh <iryna.kolhanova@gmail.com>
2026-01-07 12:14:51 -0500

    add childCare expense

--- programs/programs/urgent_needs/tx/early_intervention.py
+++ programs/programs/urgent_needs/tx/early_intervention.py
@@ -9,4 +9,6 @@ class EarlyIntervention(UrgentNeedFunction):
         # age under 5
         has_young_child = self.screen.num_children(age_max=self.max_age) > 0
         has_dev_concern = self.screen.needs_child_dev_help
-        return has_young_child and has_dev_concern
+        # Check if there are any child care expenses
+        has_child_expense = self.screen.has_expense(["childCare"])
+        return has_young_child and has_dev_concern and has_child_expense
