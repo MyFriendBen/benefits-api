a2e1653d6b4ae90878f7ccdd0b9868df992cb3be
RomannaBidnyk <romanna.bidnyk@gmail.com>
2026-01-09 15:48:42 -0500

    MFB-543: Added MA: CEOC - MassHealth Application Assistance

---
+++ programs/programs/urgent_needs/ma/masshealth_application.py
@@ -0,0 +1,23 @@
+from ..base import UrgentNeedFunction
+
+
+class MassHealthApplication(UrgentNeedFunction):
+    """
+    CEOC – MassHealth Application Assistance
+
+    Helps residents apply for free or low-cost health insurance.
+    """
+
+    dependencies = []
+
+    def eligible(self):
+
+        # Condition 1: Not having MassHealth
+        has_masshealth = any(member.insurance.mass_health for member in self.screen.household_members.all())
+
+        # Condition 2: Eligible for MassHealth
+        is_masshealth_eligible = any(
+            program["name_abbreviated"] == "ma_mass_health" and program["eligible"] for program in self.data
+        )
+
+        return is_masshealth_eligible and not has_masshealth
