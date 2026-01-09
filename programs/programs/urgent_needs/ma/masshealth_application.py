9a76dc541465bb550ff03ed19ceb197d1d98f7cd
RomannaBidnyk <romanna.bidnyk@gmail.com>
2026-01-09 16:52:33 -0500

    MFB-543: Removed extra condition - not having masshealth

--- programs/programs/urgent_needs/ma/masshealth_application.py
+++ programs/programs/urgent_needs/ma/masshealth_application.py
@@ -12,12 +12,9 @@ class MassHealthApplication(UrgentNeedFunction):
 
     def eligible(self):
 
-        # Condition 1: Not having MassHealth
-        has_masshealth = any(member.insurance.mass_health for member in self.screen.household_members.all())
-
-        # Condition 2: Eligible for MassHealth
+        # Check eligibility for MassHealth
         is_masshealth_eligible = any(
             program["name_abbreviated"] == "ma_mass_health" and program["eligible"] for program in self.data
         )
 
-        return is_masshealth_eligible and not has_masshealth
+        return is_masshealth_eligible
