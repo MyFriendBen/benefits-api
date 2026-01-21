b699d67661b222d87aaf8b439c78d095d7e33d67
nihalerdal <nihalerdall@gmail.com>
2026-01-21 14:44:01 -0800

    Add early return if user already has SNAP for performance boost

--- programs/programs/urgent_needs/ma/snap_application_assistance.py
+++ programs/programs/urgent_needs/ma/snap_application_assistance.py
@@ -19,19 +19,14 @@ class SNAPApplicationAssistance(UrgentNeedFunction):
 
         # Condition 2: Not receiving SNAP
         has_snap = self.screen.has_benefit("ma_snap")
+        if has_snap:
+            return False
 
         # Condition 3: SNAP Eligible
-        is_snap_eligible = False
-
         for program in self.data or []:
             if not isinstance(program, dict):
                 continue
+            if program.get("name_abbreviated") == "ma_snap":
+                return program.get("eligible")
 
-            if program.get("name_abbreviated") != "ma_snap":
-                continue
-
-            if program.get("eligible"):
-                is_snap_eligible = True
-                break
-
-        return is_snap_eligible and not has_snap
+        return False
