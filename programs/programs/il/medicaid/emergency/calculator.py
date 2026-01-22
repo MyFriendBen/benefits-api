c1f1f1712cc1c8d62983c7663e367655a14568c0
Caton Hauer <caton@myfriendben.org>
2026-01-22 14:48:13 -0700

    avoid hardcoding

--- programs/programs/il/medicaid/emergency/calculator.py
+++ programs/programs/il/medicaid/emergency/calculator.py
@@ -1,17 +1,17 @@
 from programs.programs.calc import MemberEligibility, ProgramCalculator, Eligibility
 from programs.programs.helpers import medicaid_eligible
+from programs.programs.il.pe.member import IlMedicaid
 import programs.programs.messages as messages
 
 
 class IlEmergencyMedicaid(ProgramCalculator):
-    # Monthly value from IL Medicaid categories (ADULT: 474), annualized
-    member_amount = 474 * 12
+    member_amount = IlMedicaid.medicaid_categories["ADULT"] * 12
     insurance_types = ["none"]
     dependencies = ["insurance"]
 
     def household_eligible(self, e: Eligibility):
-        # Does qualify for Medicaid
-        e.condition(medicaid_eligible(self.data), messages.must_have_benefit("Medicaid"))
+        # Must qualify for Medicaid
+        e.condition(medicaid_eligible(self.data))
 
     def member_eligible(self, e: MemberEligibility):
         member = e.member
