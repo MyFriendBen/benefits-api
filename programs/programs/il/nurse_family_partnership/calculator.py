44a614912e2314b4243aef83264342c124c64e33
Caton Hauer <caton@myfriendben.org>
2026-01-23 12:16:00 -0700

    updates

--- programs/programs/il/nurse_family_partnership/calculator.py
+++ programs/programs/il/nurse_family_partnership/calculator.py
@@ -2,7 +2,27 @@ from programs.programs.calc import MemberEligibility, ProgramCalculator, Eligibi
 
 
 class IlNurseFamilyPartnership(ProgramCalculator):
-    # TODO: add more context on where we got this number
+    """
+    Illinois Nurse-Family Partnership (NFP)
+
+    Pairs first-time pregnant women with registered nurses who provide support
+    from early pregnancy through the child's second birthday.
+
+    Eligibility:
+    - Pregnant (typically first-time moms, 28 weeks or less, but requirements vary by location so kept it flexible)
+    - Income <= 300% FPL OR has WIC (presumed eligibility)
+
+    Note: One partner confirmed they use 200% FPL, but WIC partners indicated flexibility
+    in practice. We use a higher cutoff (300% FPL) and note in descriptions that
+    requirements vary by location.
+
+    References:
+    - https://changent.org/what-we-do/nurse-family-partnership/
+    - https://changent.org/nfp-moms/
+    - https://changent.org/locations/
+    - CEDA (Community and Economic Development Association of Cook County) WIC Program
+    """
+
     fpl_percent = 3
     child_relationships = ["child"]
     amount = 0
@@ -15,9 +35,6 @@ class IlNurseFamilyPartnership(ProgramCalculator):
     ]
 
     def household_eligible(self, e: Eligibility):
-        # no other children
-        e.condition(self.screen.num_children(child_relationship=self.child_relationships) == 0)
-
         # income eligibility: 300% FPL or has WIC (presumed eligibility)
         income_limit = self.program.year.as_dict()[2] * self.fpl_percent
         gross_income = self.screen.calc_gross_income("yearly", ["all"])
