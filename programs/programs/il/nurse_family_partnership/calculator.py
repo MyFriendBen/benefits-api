ca147c2d55552a34f29b83eb5497aa7f935b2af1
Caton Hauer <caton@myfriendben.org>
2026-01-23 11:56:43 -0700

    move some to HH

--- programs/programs/il/nurse_family_partnership/calculator.py
+++ programs/programs/il/nurse_family_partnership/calculator.py
@@ -18,17 +18,17 @@ class IlNurseFamilyPartnership(ProgramCalculator):
         # no other children
         e.condition(self.screen.num_children(child_relationship=self.child_relationships) == 0)
 
-    def member_eligible(self, e: MemberEligibility):
-        member = e.member
-
-        # pregnant
-        e.condition(member.pregnant)
-
         # income eligibility: 300% FPL or has WIC (presumed eligibility)
-        income_limit = self.program.year.as_dict()[2] * IlNurseFamilyPartnership.fpl_percent
-        income = member.calc_gross_income("yearly", ["all"])
-        is_income_eligible = income <= income_limit
+        income_limit = self.program.year.as_dict()[2] * self.fpl_percent
+        gross_income = self.screen.calc_gross_income("yearly", ["all"])
+        is_income_eligible = gross_income <= income_limit
 
         has_wic = self.screen.has_benefit("wic")
 
         e.condition(is_income_eligible or has_wic)
+
+    def member_eligible(self, e: MemberEligibility):
+        member = e.member
+
+        # pregnant
+        e.condition(member.pregnant is True)
