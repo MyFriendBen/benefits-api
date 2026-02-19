49cd0ccdbad4bffb9b1d9c299530f52938156fdd
nihalerdal <nihalerdall@gmail.com>
2026-02-19 15:15:58 -0800

    add nc_show_211_link for nc-lanc in uiOptions

--- configuration/white_labels/nc.py
+++ configuration/white_labels/nc.py
@@ -2890,7 +2890,7 @@ class NcConfigurationData(ConfigurationData):
             "default": ["nc_show_211_link"],
             "211nc": ["no_results_more_help", "nc_show_211_link", "211nc"],
             "hfed": ["no_results_more_help", "hfed"],
-            "lanc": ["no_results_more_help", "lanc"],
+            "lanc": ["no_results_more_help", "nc_show_211_link", "lanc"],
         },
         # Deprecated: use uiOptions. Remove as part of MFB-635.
         "featureFlags": {
