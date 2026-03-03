6bafe4b4af8c85d358d72faad836eed79afc008a
IrynaKolh <iryna.kolhanova@gmail.com>
2026-03-02 20:33:48 -0500

    Remove redundant eligibility checks from NationalDiaperBankNetwork

--- programs/programs/urgent_needs/tx/diaper_bank.py
+++ programs/programs/urgent_needs/tx/diaper_bank.py
@@ -11,31 +11,8 @@ class NationalDiaperBankNetwork(UrgentNeedFunction):
     More info: https://nationaldiaperbanknetwork.org/member-directory/
     """
 
-    dependencies = ["age", "county"]
+    dependencies = ["age"]
     max_age = 4
-    eligible_counties = [
-        "Travis",
-        "Dallas",
-        "El Paso",
-        "Tarrant",
-        "Galveston",
-        "Brazoria",
-        "Collin",
-        "Bexar",
-        "McLennan",
-    ]
 
     def eligible(self) -> bool:
-        # Check if county is eligible
-        is_eligible_county = self.screen.county in self.eligible_counties
-        if not is_eligible_county:
-            return False
-
-        # age under 5
-        has_young_child = self.screen.num_children(age_max=self.max_age) > 0
-        # coerce possible None to False
-        has_needs_baby_supplies = bool(self.screen.needs_baby_supplies)
-
-        # Check if there are any child support expenses
-        has_child_support_expense = bool(self.screen.has_expense(["childSupport", "childCare"]))
-        return has_young_child and has_needs_baby_supplies and has_child_support_expense
+        return self.screen.num_children(age_max=self.max_age) > 0
