0f5b403a83d41128e8b5b376fd61ffb8a90b56b4
Caton Hauer <caton@myfriendben.org>
2026-03-16 12:56:21 -0600

    fix merge conflict

--- programs/programs/tx/__init__.py
+++ programs/programs/tx/__init__.py
@@ -3,9 +3,6 @@ from ..calc import ProgramCalculator
 from .ssdi.calculator import TxSsdi
 
 tx_calculators: dict[str, type[ProgramCalculator]] = {
-<<<<<<< caton/mfb-676-tx-add-ssdi-social-security-disability-insurance
     "tx_ssdi": TxSsdi,
-=======
     "tx_ccad": TxCcad,
->>>>>>> main
 }
