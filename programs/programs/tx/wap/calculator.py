9398d0711b2dc40f15d208c5e3e925758c911c55
Pat Wey <pat@myfriendben.org>
2026-03-31 12:49:49 -0700

    TX WAP: rename categorical_eligible to categorically_eligible

--- programs/programs/tx/wap/calculator.py
+++ programs/programs/tx/wap/calculator.py
@@ -14,11 +14,11 @@ class TxWap(ProgramCalculator):
 
     def household_eligible(self, e: Eligibility):
         # Categorical eligibility: SSI, TANF, or SNAP bypass the income test
-        categorical_eligible = (
+        categorically_eligible = (
             self.screen.has_benefit("ssi") or self.screen.has_benefit("tanf") or self.screen.has_benefit("snap")
         )
 
-        if categorical_eligible:
+        if categorically_eligible:
             e.condition(True, messages.presumed_eligibility())
         else:
             gross_income = self.screen.calc_gross_income("yearly", ["all"])
