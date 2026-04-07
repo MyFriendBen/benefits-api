efcae545489ae5e36219a2c2b7939e51550c8a19
Pat Wey <pat@myfriendben.org>
2026-04-07 12:46:21 -0700

    Fix black formatting in wa/pe/spm.py (black 26.3.0)

--- programs/programs/wa/pe/spm.py
+++ programs/programs/wa/pe/spm.py
@@ -1,7 +1,6 @@
 import programs.programs.policyengine.calculators.dependencies as dependency
 from programs.programs.federal.pe.spm import Snap
 
-
 # TODO: add state specific SPM calculators from PE here
 # TODO: add dependency.household.WaStateCode dependency
 
