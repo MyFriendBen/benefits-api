9d4bced3b45e19ff61791c25bf1f8fd06748fd1f
Elliott Waissbluth <73327197+elliottwaissbluth@users.noreply.github.com>
2026-05-08 10:35:17 -0700

    Implement WaLifeline custom calculator

--- programs/programs/wa/__init__.py
+++ programs/programs/wa/__init__.py
@@ -1,4 +1,5 @@
 from .csfp.calculator import WaCsfp
+from .lifeline.calculator import WaLifeline
 from .wsos_bas.calculator import WaWsosBas
 from .wsos_cts.calculator import WaWsosCts
 from .wsos_grd.calculator import WaWsosGrd
@@ -8,6 +9,7 @@ from .ssdi.calculator import WaSsdi
 
 wa_calculators: dict[str, type[ProgramCalculator]] = {
     "wa_csfp": WaCsfp,
+    "wa_lifeline": WaLifeline,
     "wa_ssdi": WaSsdi,
     "wa_wsos_bas": WaWsosBas,
     "wa_wsos_cts": WaWsosCts,
