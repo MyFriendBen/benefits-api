f34989d0c33fb19eeb2b865e1b82be32e5b7aa20
David Stoll <stoll33david@gmail.com>
2026-05-29 14:38:26 -0600

    updating to support health insurance and benefits

--- programs/programs/wa/wap/calculator.py
+++ programs/programs/wa/wap/calculator.py
@@ -31,8 +31,6 @@ class WaWap(ProgramCalculator):
     ]
 
     def household_eligible(self, e: Eligibility):
-
-        # If currently receiving weatherization, uesr is ineligible
         if self.screen.has_benefit("wa_wap"):
             e.condition(False, messages.must_not_have_benefit("wa_wap"))
 
