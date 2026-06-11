1fa408fde7ad63a49c64a0764881984093fba0e3
nihalerdal <nihalerdall@gmail.com>
2026-06-11 13:13:54 -0700

    Remove unused function

--- integrations/util/cache.py
+++ integrations/util/cache.py
@@ -28,10 +28,6 @@ class Cache:
             level = "error" if self.invalid else "warning"
             capture_exception(e, level=level)
 
-    def is_valid(self) -> bool:
-        """Returns True if the cache has been successfully populated at least once."""
-        return not self.invalid
-
     def save(self, data):
         self.data = data
 
