bec7e0710068c92230946caa097172abf612d8e0
Caton Hauer <caton@myfriendben.org>
2026-07-07 18:55:26 -0600

    Register ks_ctc PolicyEngine program (federal Ctc, as-is)

--- programs/programs/ks/pe/__init__.py
+++ programs/programs/ks/pe/__init__.py
@@ -1,6 +1,7 @@
 import programs.programs.ks.pe.spm as spm
 import programs.programs.ks.pe.tax as tax
 import programs.programs.ks.pe.member as member
+from programs.programs.federal.pe.tax import Ctc
 from programs.programs.policyengine.calculators.base import PolicyEngineCalulator
 
 ks_member_calculators = {
@@ -11,6 +12,8 @@ ks_member_calculators = {
 ks_tax_unit_calculators = {
     "ks_eitc": tax.Kseitc,
     "ks_cdcc": tax.KsCdcc,
+    # Federal Child Tax Credit, used as-is (no KS variance). Mirrors wa_ctc.
+    "ks_ctc": Ctc,
 }
 
 ks_spm_calculators = {
