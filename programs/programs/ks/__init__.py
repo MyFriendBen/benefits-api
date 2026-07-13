4fcf58b21940d01c5a41cc38b12120f1035497c1
david-j-stoll <stoll33david@gmail.com>
2026-07-13 15:06:20 -0600

    Implement KsPromiseAct custom calculator

--- programs/programs/ks/__init__.py
+++ programs/programs/ks/__init__.py
@@ -2,9 +2,11 @@ from ..calc import ProgramCalculator
 from .ssdi.calculator import KsSsdi
 from .k40h.calculator import KsK40h
 from .working_healthy.calculator import KsWorkingHealthy
+from .promise_act.calculator import KsPromiseAct
 
 ks_calculators: dict[str, type[ProgramCalculator]] = {
     "ks_ssdi": KsSsdi,
     "ks_k40h": KsK40h,
     "ks_working_healthy": KsWorkingHealthy,
+    "ks_promise_act": KsPromiseAct,
 }
