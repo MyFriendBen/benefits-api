515c44d5a64df72c19d4de70a3ca9f1044973b62
david-j-stoll <stoll33david@gmail.com>
2026-07-30 15:10:43 -0600

    Implement MoNslp PolicyEngine program class

--- programs/programs/mo/pe/__init__.py
+++ programs/programs/mo/pe/__init__.py
@@ -1,10 +1,16 @@
 import programs.programs.mo.pe.member as member
+import programs.programs.mo.pe.spm as spm
 from programs.programs.policyengine.calculators.base import PolicyEngineCalulator
 
 mo_member_calculators = {
     "mo_wic": member.MoWic,
 }
 
+mo_spm_calculators = {
+    "mo_nslp": spm.MoNslp,
+}
+
 mo_pe_calculators: dict[str, type[PolicyEngineCalulator]] = {
     **mo_member_calculators,
+    **mo_spm_calculators,
 }
