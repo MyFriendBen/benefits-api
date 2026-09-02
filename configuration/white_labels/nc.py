0dee77f159bb69f3392918c46fda6f547bafc40d
RomannaBidnyk <romanna.bidnyk@gmail.com>
2026-09-02 10:07:51 -0400

    MFB-824: fix grammar in nc.py per CodeRabbit comment

--- configuration/white_labels/nc.py
+++ configuration/white_labels/nc.py
@@ -2660,7 +2660,7 @@ class NcConfigurationData(ConfigurationData):
         "noResultMessage": {
             "default": {
                 "_label": "noResultMessage.nc",
-                "_default_message": "It looks like you may not qualify for benefits included in MyFriendBen at this time. If you indicated need for an immediate resource, please click on the “Additional Resources” tab.",
+                "_default_message": "It looks like you may not qualify for benefits included in MyFriendBen at this time. If you indicated a need for an immediate resource, please click on the “Additional Resources” tab.",
             }
         },
         "defaultLanguage": {
