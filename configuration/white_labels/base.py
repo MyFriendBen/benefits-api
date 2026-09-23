3c3f8860670f4ba01b486d39f7432d17776387b5
RomannaBidnyk <romanna.bidnyk@gmail.com>
2026-09-23 12:58:34 -0400

    fixed quote-style

--- configuration/white_labels/base.py
+++ configuration/white_labels/base.py
@@ -566,7 +566,7 @@ class ConfigurationData:
         "noResultMessage": {
             "default": {
                 "_label": "noResultMessage",
-                "_default_message": 'It looks like you may not qualify for benefits included in MyFriendBen at this time. You can find other support matched to your needs in the Additional Resources tab. If you need help right away, go to the Immediate Help tab',
+                "_default_message": "It looks like you may not qualify for benefits included in MyFriendBen at this time. You can find other support matched to your needs in the Additional Resources tab. If you need help right away, go to the Immediate Help tab",
             },
         },
     }
